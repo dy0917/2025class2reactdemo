d5004df46e2624161c0be7e3e6833792020588cc
dy0917 <huangkingsley@gmail.com>
2025-10-08 22:51:28 +1300

    update login page

--- routeDemo/src/Pages/Login.jsx
+++ routeDemo/src/Pages/Login.jsx
@@ -2,8 +2,6 @@ import { useNavigate, Outlet } from "react-router-dom";
 import SubscribeForm from "../Components/SubcribeForm";
 
 export default function Login() {
-  // Save in pages/Homepage.jsx
-  const navigate = useNavigate();
   return (
     <div className="Homepage">
       <SubscribeForm></SubscribeForm>
