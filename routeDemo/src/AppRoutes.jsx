79586da3b5c5d7f2f4781badd79418f25aca4c90
dy0917 <huangkingsley@gmail.com>
2025-10-08 21:26:08 +1300

    add pages

--- routeDemo/src/AppRoutes.jsx
+++ routeDemo/src/AppRoutes.jsx
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
 import DashboardPage from "./Pages/DashboardPage";
 import DashboardMessages from "./Pages/DashboardMessages";
 import DashboardTasks from "./Pages/DashboardTasks";
+import PageNotFound from "./Pages/PageNotFound";
+
 export default function AppRoutes(props) {
   return (
     <Routes>
@@ -11,6 +14,8 @@ export default function AppRoutes(props) {
         <Route path="messages" element={<DashboardMessages />} />
         <Route path="tasks" element={<DashboardTasks />} />
       </Route>
+      <Route path="/about" element={<AboutPage {...props} />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
---
+++ routeDemo/src/Pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div className="Homepage">
+      <h1>PageNotFound</h1>
+    </div>
+  );
+}
